2e2670b0f3b21e76d0bcb3824a63f4b2e719d879
unknown <felipereis.live@gmail.com>
2024-12-02 20:34:50 -0300

    Criando operação para conta

--- js/byteBank.js
+++ js/byteBank.js
@@ -11,5 +11,33 @@ elementoFormulario.addEventListener("submit", function () {
         alert("Por favor, preencha todos os campos da transação!")
         return;
     }
+
+    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
+    const inputValor = elementoFormulario.querySelector("#valor");
+    const inputData = elementoFormulario.querySelector("#data");
+
+    let tipoTransacao = inputTipoTransacao.value;
+    let valor = inputValor.value;
+    let data = inputData.value;
+
+    if (tipoTransacao == "Depósito") {
+        saldo += valor;
+    }else if(tipoTransacao == "Transferência" || tipoTransacao == "Pagamento boleto") {
+        saldo -= valor;
+    }else {
+        alert("Tipo de Transação é inválida!")
+        return;
+    }
+
+    elementosaldo.textContent = saldo;
+
+    const novaTransacao = {
+        tipoTransacao: tipoTransacao,
+        valor: valor,
+        data: data
+    }
+
+    elementoFormulario.reset();
 });
+
 
