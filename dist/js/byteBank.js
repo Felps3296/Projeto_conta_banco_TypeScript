e8bb58395e7e5cfd049dc03f8eb37df951e5acb0
unknown <felipereis.live@gmail.com>
2024-12-04 18:09:47 -0300

    Organizando diretorios e configurando tsconfig.json

---
+++ dist/js/byteBank.js
@@ -0,0 +1,36 @@
+let saldo = 3000;
+const elementosaldo = document.querySelector(".saldo-valor .valor");
+if (elementosaldo != null) {
+    elementosaldo.textContent = saldo.toString();
+}
+const elementoFormulario = document.querySelector(".block-nova-transacao form");
+elementoFormulario.addEventListener("submit", function () {
+    event.preventDefault();
+    if (!elementoFormulario.checkVisibility()) {
+        alert("Por favor, preencha todos os campos da transação!");
+        return;
+    }
+    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
+    const inputValor = elementoFormulario.querySelector("#valor");
+    const inputData = elementoFormulario.querySelector("#data");
+    let tipoTransacao = inputTipoTransacao.value;
+    let valor = inputValor.valueAsNumber;
+    let data = new Date(inputData.value);
+    if (tipoTransacao == "Depósito") {
+        saldo += valor;
+    }
+    else if (tipoTransacao == "Transferência" || tipoTransacao == "Pagamento boleto") {
+        saldo -= valor;
+    }
+    else {
+        alert("Tipo de Transação é inválida!");
+        return;
+    }
+    elementosaldo.textContent = saldo.toString();
+    const novaTransacao = {
+        tipoTransacao: tipoTransacao,
+        valor: valor,
+        data: data
+    };
+    elementoFormulario.reset();
+});
